51d8623f42f9c9b6be756ce7c7cfc9123571426e
Noyan <noyan.alimov@gmail.com>
2022-12-04 04:20:36 +0000

    fixed trpc context error

--- app/server/context.ts
+++
@@ -1,9 +0,0 @@
-import { inferAsyncReturnType } from '@trpc/server'
-import * as trpcNext from '@trpc/server/adapters/next'
-import { prisma } from './db'
- 
-export async function createContext(opts: trpcNext.CreateNextContextOptions) {
-    return { prisma }
-}
-
-export type Context = inferAsyncReturnType<typeof createContext>
